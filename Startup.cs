917cf9476ee7b81aa43fc44297ee8777e91ff823
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-10-17 08:29:20 -0400

    Agregando el registro para Personas

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace Blazor_Detalle
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazoredToast();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
