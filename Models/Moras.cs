e29c8f696160499aae07bc9da62b1a556c7227f1
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-10-17 07:44:22 -0400

    Arreglos en Entidades y Agregando Personas

--- Models/Moras.cs
+++ Models/Moras.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blazor_Detalle{
 
     public class Moras{
-        [Key] public int MorasId { get; set; }
-        public DateTime Fecha { get; set;}
+        [Key] public int MoraId { get; set; }
+        [Required(ErrorMessage = "Debe poner una Fecha")] public DateTime Fecha { get; set;}
         public float Total { get; set;}
+
+        [ForeignKey("MoraId")] 
+        public virtual List<MorasDetalle> MoraDetalle { get; set;} = new List<MorasDetalle>();
     }
 }
--- Models/MorasDetalle.cs
+++ Models/MorasDetalle.cs
@@ -6,8 +6,8 @@ namespace Blazor_Detalle{
     public class MorasDetalle{
 
         [Key] public int MoraDetalleId { get; set; }
-        public int MoraId { get; set;}
-        public int PrestamoId { get; set;}
+        [Required(ErrorMessage = "Es obligatorio poner el Id de la Mora")] public int MoraId { get; set;}
+        [Required(ErrorMessage = "Es obligatorio poner el Id del Prestamo")] public int PrestamoId { get; set;}
         public float Valor { get; set;}
     }
 
